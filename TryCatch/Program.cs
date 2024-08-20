bf65ec327a6e1221811afe8325f731f0d61a90a1
sinan <sinan@SINAN-SENKUL>
2024-08-20 11:02:41 +0300

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        void squareFinder()
+        {
+            try
+            {
+                Console.WriteLine("bir sayı giriniz");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"girdiğiniz sayının karesi: {num * num}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("format hatası: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("beklenmeyen bir hata oluştu: " + ex.Message);
+            }
+        }
+        squareFinder();
+    }
+}
